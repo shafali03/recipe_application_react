3e6229720b9cbc6c516b9fbc60104022905b881a
shafali03 <shaf03sm@gmail.com>
2020-02-20 23:05:36 +0000

    feat: Created variables so I pull all the information from the objects

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import RecipeList from './RecipeList'
 
 function App() {
   return (
-    <RecipeList recipe={sampleRecipes}/>
+    <RecipeList recipes={sampleRecipes}/>
   )
 }
 
@@ -14,14 +14,14 @@ const sampleRecipes = [
     name: 'Plain Chicken',
     servings: 3,
     cookTime: '1:45',
-    instruction: '1. Put pepper on Chicken \n2. Put marinate with lemon and herb \n3. Put in oven'
+    instructions: '1. Put pepper on Chicken \n2. Add marinate with lemon and herb \n3. Put in oven'
   },
   {
     id: 2,
-    name: 'Plain rice',
+    name: 'Plain Rice',
     servings: 5,
     cookTime: '0:25',
-    instruction: '1. Mix Chicken with rice\n2. Add spices to rice \n3. cook rice'
+    instructions: '1. Mix Chicken with rice\n2. Add spices to rice \n3. cook rice'
 
   }
 ]
