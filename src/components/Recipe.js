8a1634097441db3c33ba8df969c414386e2e9cec
shafali03 <shaf03sm@gmail.com>
2020-02-26 11:02:38 +0000

    add class names to recipes

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -18,21 +18,21 @@ export default function Recipe(props) {
           <button className="btn btn--danger">Delete</button>
         </div>
       </div>
-      <div>
-        <span>Cook Time:</span>
-        <span>{cookTime}</span>
+      <div className="recipe__row">
+        <span className="recipe__label">Cook Time:</span>
+        <span className="recipe__value">{cookTime}</span>
       </div>
-      <div>
-        <span>Servings:</span>
-        <span>{servings}</span>
+      <div className="recipe__row">
+        <span className="recipe__label">Servings:</span>
+        <span className="recipe__value">{servings}</span>
       </div>
-      <div>
-        <span>Instructions:</span>
-        <div>{instructions}</div>
+      <div className="recipe__row">
+        <span className="recipe__label">Instructions:</span>
+        <div className="recipe__value">{instructions}</div>
       </div>
-      <div>
-        <span>Ingredients:</span>
-        <div>
+      <div className="recipe__row">
+        <span className="recipe__label">Ingredients:</span>
+        <div className="recipe__value">
         <IngredientList ingredients={ingredients}/>
         </div>
       </div>
