009e89145338fbbc7c40f3f88db8c7928049bf3b
shafali03 <shaf03sm@gmail.com>
2020-02-26 16:24:36 +0000

    Added recipe add button interactivity

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RecipeList from './RecipeList'
 import '../css/app.css'
+import uuidv4 from 'uuid/v4'
 
 
 function App() {
+  const [recipes, setRecipe] = useState (sampleRecipes)
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [
+        { id: uuidv4(), name: 'Name', amount: '1Tbps' }
+      ]
+    }
+  
+    setRecipe([...recipes, newRecipe])
+  }
+
+
   return (
-    <RecipeList recipes={sampleRecipes}/>
+    <RecipeList 
+    recipes={recipes}
+     handleRecipeAdd={handleRecipeAdd} 
+    />
   )
 }
+
+
 
 const sampleRecipes = [
   {
