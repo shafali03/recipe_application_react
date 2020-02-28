e13bf4a4f30f16d99dd6652901c9f5cb77903fc2
shafali03 <shaf03sm@gmail.com>
2020-02-28 09:45:55 +0000

    added all class name for recipe section

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -4,18 +4,48 @@ import RecipeIngredientEdit from './RecipeIngredientEdit'
 export default function RecipeEdit() {
   return (
     <div className="recipe-edit">
-     <div>
-       <button>&times;</button>
+     <div className="recipe-edit__remove-button-container">
+       <button className="btn recipe-edit__remove-button">&times;</button>
      </div>
-     <div>
-       <label htmlFor="name">Name</label>
-       <input type="text" name="name" id="name" />
-       <label htmlFor="cookTime">Cook Time</label>
-       <input type="text" name="cookTime" id="cookTime" />
-       <label htmlFor="servings">Servings</label>
-       <input type="text" min="1" name="servings" id="servings" />
-       <label htmlFor="instructions">Instructions</label>
-       <textarea name="instructions" id="instruction"></textarea>
+     <div className="recipe-edit__details-grid">
+       <label htmlFor="name"
+         className="recipe-edit__label">
+         Name
+        </label>
+       <input 
+         type="text" 
+         name="name" 
+         id="name" 
+         className="recipe-edit__input" />
+       <label 
+         htmlFor="cookTime"
+         className="recipe-edit__label">
+         Cook Time
+       </label>
+       <input 
+         type="text" 
+         name="cookTime" 
+         id="cookTime" />
+       <label 
+         htmlFor="servings"
+         className="recipe-edit__label">
+         Servings
+       </label>
+       <input 
+         type="text" 
+         min="1" 
+         name="servings" 
+         id="servings" />
+       <label 
+         htmlFor="instructions"
+         className="recipe-edit__label">
+         instructions
+       </label>
+       <textarea 
+       name="instructions"
+       className="recipe-edit__input"
+       id="instruction">
+       </textarea>
      </div>
      <br />
      <label>Ingredients</label>
